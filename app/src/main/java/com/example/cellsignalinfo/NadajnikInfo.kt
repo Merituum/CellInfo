8563d45cfbd42f828ce50d827ab916cd1c9777d9
Bartosz Siwik <61067659+Merituum@users.noreply.github.com>
2025-06-11 19:38:14 +0200

    Update NadajnikInfo.kt
    fixed some unnecessary comments#1

--- app/src/main/java/com/example/cellsignalinfo/NadajnikInfo.kt
+++ app/src/main/java/com/example/cellsignalinfo/NadajnikInfo.kt
@@ -1,4 +1,4 @@
-package com.example.cellsignalinfo // Zmień na swoją nazwę pakietu
+package com.example.cellsignalinfo 
 
 import com.google.gson.annotations.SerializedName
 
@@ -6,6 +6,6 @@ data class NadajnikInfo(
     @SerializedName("siec_id") val siecId: String?,
     @SerializedName("miejscowosc") val miejscowosc: String?,
     @SerializedName("lokalizacja") val lokalizacja: String?,
-    @SerializedName("LATIuke") val latitude: Double?,    // Zwróć uwagę na wielkość liter w JSON
-    @SerializedName("LONGuke") val longitude: Double?   // Zwróć uwagę na wielkość liter w JSON
+    @SerializedName("LATIuke") val latitude: Double?,    
+    @SerializedName("LONGuke") val longitude: Double?   
 )
