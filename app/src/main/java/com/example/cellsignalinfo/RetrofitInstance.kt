9f34d3e9ae8e618e97c2a34a56f01aef1b7c4783
Bartosz Siwik <61067659+Merituum@users.noreply.github.com>
2025-06-11 19:39:13 +0200

    Update RetrofitInstance.kt
    fixed some unnecessary comments#3

--- app/src/main/java/com/example/cellsignalinfo/RetrofitInstance.kt
+++ app/src/main/java/com/example/cellsignalinfo/RetrofitInstance.kt
@@ -5,13 +5,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    // ZMIEŃ TEN ADRES URL na adres Twojego lokalnego serwera!
-    // Jeśli testujesz na emulatorze Androida, użyj 10.0.2.2 aby odnieść się do localhost Twojego komputera.
-    // Jeśli testujesz na fizycznym urządzeniu, użyj lokalnego adresu IP Twojego komputera w sieci Wi-Fi (np. 192.168.1.100).
-    // Upewnij się, że serwer PHP jest uruchomiony i dostępny pod tym adresem i portem.
-    // Jeśli Twój skrypt PHP jest w podkatalogu, np. "api", to dodaj to do BASE_URL np. "http://10.0.2.2/api/"
-    // lub dodaj do ścieżki w NadajnikiApiService @GET("api/nadajniki.php")
-   private const val BASE_URL = "http://10.0.2.2/" // Przykładowy adres dla emulatora, jeśli PHP jest w roocie serwera
+ 
+   
+   private const val BASE_URL = "http://10.0.2.2/" 
     //private const val BASE_URL = "http://192.168.137.1/"
     val api: NadajnikiApiService by lazy {
         Retrofit.Builder()
