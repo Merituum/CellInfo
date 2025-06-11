8e7f305614b96afc9550a7f5ed993983c3e85184
Bartosz Siwik <61067659+Merituum@users.noreply.github.com>
2025-06-11 19:39:26 +0200

    Update NadajnikiApiService.kt
    fixed some unnecessary comments#4

--- app/src/main/java/com/example/cellsignalinfo/NadajnikiApiService.kt
+++ app/src/main/java/com/example/cellsignalinfo/NadajnikiApiService.kt
@@ -1,9 +1,9 @@
-package com.example.cellsignalinfo // Zmień na swoją nazwę pakietu
+package com.example.cellsignalinfo 
 
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface NadajnikiApiService {
-    @GET("get_nadajniki.php") // Np. "api/nadajniki.php" lub po prostu "nadajniki.php" jeśli jest w roocie
+    @GET("get_nadajniki.php") 
     suspend fun getNadajniki(): Response<List<NadajnikInfo>>
 }
